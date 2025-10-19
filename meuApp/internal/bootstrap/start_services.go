e520d3c2ec5c83d83aa060ead95a1cec636a38da
Valdir Barbosa <valdirsb@gmail.com>
2025-10-19 00:05:34 -0300

    refactor: Remove legacy bootstrap files and consolidate service registration

--- meuApp/internal/bootstrap/start_services.go
+++
@@ -1,9 +0,0 @@
-package bootstrap
-
-// StartServices era usado para registrar os services antigos
-// Agora usamos StartApplicationServices que registra os Application Services (CQRS)
-// Este arquivo é mantido vazio para compatibilidade, mas pode ser removido no futuro
-func (s *Bootstrap) StartServices() {
-// Todos os services agora são registrados via StartApplicationServices()
-// que implementa o padrão CQRS com Commands e Queries
-}
